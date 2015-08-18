287985a485c0d5c7dfafa2b8193265a0af1834d8
WebCrew <palareas@gmail.com>
2015-08-18 17:13:14 +0200

    what we`ve done so far
    this is what I`ve done so far

---
+++ views/layouts/head.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" href="<?PHP echo Yii::app()->theme->baseUrl; ?>/css/normalize.css" />
+<link rel="stylesheet" type="text/css" href="<?PHP echo Yii::app()->theme->baseUrl; ?>/css/icons.css" />
+<link rel="stylesheet" type="text/css" href="<?PHP echo Yii::app()->theme->baseUrl; ?>/css/site.css" />
+<link rel="stylesheet" type="text/css" href="<?PHP echo Yii::app()->theme->baseUrl; ?>/css/login.css" />
+<script src="<?PHP echo Yii::app()->theme->baseUrl; ?>/js/bgvideo.min.js"></script>
+<script type="text/javascript" src="<?PHP echo Yii::app()->theme->baseUrl; ?>/js/formslider.js"></script>
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script>
+// use like this, nothing else needed
+	bgVideo({
+	sources: ['<?PHP echo Yii::app()->theme->baseUrl; ?>/media/bg.mp4', '<?PHP echo Yii::app()->theme->baseUrl; ?>/media/bg.webm'],
+	img: '<?PHP echo Yii::app()->theme->baseUrl; ?>/media/bg.jpg'
+	});
+</script>
